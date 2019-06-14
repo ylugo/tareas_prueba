62170fa6810fc0746048b3dee31105be0aab2272
unknown <yelua10@hotmail.com>
2019-06-14 00:34:42 -0500

    porfin

---
+++ app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <h1>{{title}}</h1>
+    <input type="text" name="" id="">
+   <router-link to="/servicios">servicios</router-link>
+   <componente />
+   <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+
+export default
+ {
+  name: 'App',
+  data () {
+    return {
+      title:"probando"
+      //
+    }
+  }
+ }
+
+</script>
---
+++ app/src/views/Servicios.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>nueva ruta</h1>
+    <componente />
+    </div>
+</template>
+
+<script>
+  import Componente from '../components/Componente'
+
+  export default {
+    components: {
+      Componente
+    }
+  }
+</script>
+<style>
+
+</style>
+
